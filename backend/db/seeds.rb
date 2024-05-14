180e8064f358702317cf43a12e6fd085a7e090c3
ShishidoYuuki <yuuki0206nano@gmail.com>
2024-05-14 21:55:00 +0900

    バックエンドで作成したGETリクエストを送信できるようにする
    - db/seed.rbで仮データの作成
    - post_controllerをcontroller配下からcontroller/v1に移動
    
     Please enter the commit message for your changes. Lines starting

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# postの内容
+10.times do |n|
+Post.create(
+    title: "test#{n}",
+    content: "seedで作成したデータです"
+)
+end
