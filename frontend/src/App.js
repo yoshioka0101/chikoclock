2b99c573f960119b83d20656f8d98371ffb49acf
yoshioka0101 <yuuki0205nano@gmail.com>
2024-05-15 07:59:18 +0900

    WIP:reactの表示部分の修正

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,58 @@
+import React from 'react'; // Reactをインポート
+import axios from 'axios'; // axiosをインポート
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [] //空の初期値
+        };
+    }
+
+    componentDidMount() {
+        const axiosInstance = axios.create({
+            baseURL: process.env.REACT_APP_DEV_API_URL,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+            responseType: 'json'
+        });
+
+        axiosInstance.get('/posts')
+            .then(results => {
+                console.log(results);
+                this.setState({
+                    posts: results.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1>Hello React</h1>
+                    <h1>Posts</h1>
+                    <ul>
+                        {this.state.posts.length > 0 ? (
+                            this.state.posts.map(post => (
+                                <li key={post.id}>{post.title}</li>
+                            ))
+                        ) : (
+                            <li>No posts available</li>
+                        )}
+                    </ul>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
