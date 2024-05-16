df44fc271ac20a80847598c7fe21cbb7ec71b9dd
yoshioka0101 <yuuki0205nano@gmail.com>
2024-05-17 08:45:21 +0900

    WIP:Googlemapを利用するための設定

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :posts, only: [:index, :shoe, :create, :update, :destroy]
       resources :users, only: [:index, :create]
+      get 'places/search', to: 'places#search'
+
     end
 end
