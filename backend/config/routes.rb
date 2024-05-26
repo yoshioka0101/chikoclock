6cbb7597b5952c992759724de800e48ec7900ccb
yuki.shishido <yuki.shishido@optim.co.jp>
2024-05-27 08:11:11 +0900

    不要になった

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :posts, only: [:index, :show, :create, :update, :destroy]
       resources :users, only: [:index, :create]
-      get 'places/search', to: 'places#search'
     end
 end
