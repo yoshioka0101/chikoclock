11db9d79713d77eefb6a12392b25349fb38e00a6
yuki.shishido <yuki.shishido@optim.co.jp>
2024-05-27 08:11:11 +0900

    不要になった

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import GoogleMapsSearch from './components/GoogleMapsSearch';
 import PostForm from './components/PostForm';
 import PostSuccess from './components/PostSuccess';
 import PostList from './components/PostList';
@@ -13,14 +12,12 @@ const App = () => {
                 <nav>
                     <ul>
                         <li><Link to="/">Home</Link></li>
-                        <li><Link to="/place">Place Search</Link></li>
                         <li><Link to="/post/new">Create Post</Link></li>
                         <li><Link to="/posts">Post List</Link></li> {/* Post List へのリンクを追加 */}
                     </ul>
                 </nav>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/place" element={<GoogleMapsSearch />} />
                     <Route path="/post/new" element={<PostForm />} />
                     <Route path="/post/success" element={<PostSuccess />} />
                     <Route path="/posts" element={<PostList />} /> {/* PostList コンポーネントのルートを追加 */}
