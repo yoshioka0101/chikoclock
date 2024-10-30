1357284c8d6d97c3cf09713006e3d60e5874ccdd
yuki.shishido <yuki.shishido@optim.co.jp>
2024-10-31 03:17:30 +0900

    コンフリクトの解消

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import PostForm from './components/PostForm';
 import PostSuccess from './components/PostSuccess';
@@ -15,22 +15,127 @@ const App = () => {
         signOut();
         alert('サインアウトしました');
     };
+    const [lines, setLines] = useState([]);
+    const [message, setMessage] = useState('');  // メッセージ用
+    const [error, setError] = useState(null);
+    const [selectedArea, setSelectedArea] = useState('北海道');  // デフォルトエリア
+
+    const handleAreaChange = (e) => {
+        setSelectedArea(e.target.value);
+    };
+
+    const fetchTrainStatus = () => {
+        // 選択された地域に応じてAPIを呼び出す
+        fetch(`http://localhost:3000/v1/train_status?area=${selectedArea}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTPエラー: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.message) {
+                    setMessage(data.message);
+                    setLines([]);  // 遅延情報がない場合は空に設定
+                } else {
+                    setMessage('');
+                    setLines(data);  // 正常にデータがあればセット
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setError('現在データの取得に問題が生じております。再度お試しください。');
+            });
+    };
 
     return (
         <Router>
             <div className="App">
                 <nav>
                     <ul>
-                        <li><Link to="/">Home</Link></li>
+                    <li><Link to="/">Home</Link></li>
                         <li><Link to="/post/new">新規作成</Link></li>
                         <li><Link to="/posts">投稿一覧</Link></li>
                         <li><Link to="/login">ログイン</Link></li>
                         <li><Link to="/signup">新規登録</Link></li>
-                        <li><button onClick={handleSignOut}>ログアウト</button></li>
-                    </ul>
+                        <li><button onClick={handleSignOut}>ログアウト</button></li>                    </ul>
                 </nav>
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path="/" element={
+                        <div>
+                            <h1>Welcome to the Home Page</h1>                                            
+                            <div>
+                                <form>
+                                    <label>
+                                        <input type="radio" value="北海道" checked={selectedArea === '北海道'} onChange={handleAreaChange} />
+                                        北海道
+                                    </label>
+                                    <label>
+                                        <input type="radio" value="東北" checked={selectedArea === '東北'} onChange={handleAreaChange} />
+                                        東北
+                                    </label>
+                                    <label>
+                                        <input type="radio" value="首都圏" checked={selectedArea === '首都圏'} onChange={handleAreaChange} />
+                                        首都圏
+                                    </label>
+                                    <label>
+                                        <input type="radio" value="中部" checked={selectedArea === '中部'} onChange={handleAreaChange} />
+                                        中部
+                                    </label>
+                                    <label>
+                                        <input type="radio" value="関西" checked={selectedArea === '関西'} onChange={handleAreaChange} />
+                                        関西
+                                    </label>
+                                    <label>
+                                        <input type="radio" value="中国" checked={selectedArea === '中国'} onChange={handleAreaChange} />
+                                        中国
+                                    </label>
+                                    <label>
+                                        <input type="radio" value="四国" checked={selectedArea === '四国'} onChange={handleAreaChange} />
+                                        四国
+                                    </label>
+                                    <label>
+                                        <input type="radio" value="九州" checked={selectedArea === '九州'} onChange={handleAreaChange} />
+                                        九州
+                                    </label>
+                                </form>
+                                <button onClick={fetchTrainStatus}>選択</button>
+                            </div>
+                            {error ? (  // エラーが発生した場合
+                                <div>{error}</div>
+                            ) : (
+                                <div>
+                                    <h2>列車の運行状況 ({selectedArea})</h2>
+                                    {message ? (
+                                        <p>{message}</p>
+                                    ) : (
+                                        <table>
+                                            <thead>
+                                                <tr>
+                                                    <th>路線</th>
+                                                    <th>状況</th>
+                                                    <th>詳細</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {lines.map((line, index) => (
+                                                <tr key={index}>
+                                                    <td>
+                                                    <a href={line.url} target="_blank" rel="noopener noreferrer">
+                                                        {line.line}
+                                                    </a>
+                                                    </td>
+                                                    <td>{line.status}</td>
+                                                    <td>{line.details}</td>
+                                                </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    )}
+                                </div>
+                            )}
+                        </div>
+                    } />
                     <Route path="/post/new" element={<PostForm />} />
                     <Route path="/post/success" element={<PostSuccess />} />
                     <Route path="/posts" element={<PostList />} />
@@ -43,7 +148,5 @@ const App = () => {
         </Router>
     );
 };
-
-const Home = () => <div>Welcome to the Home Page</div>;
 
 export default App;
